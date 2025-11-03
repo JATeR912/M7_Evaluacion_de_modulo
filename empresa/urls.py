fc4852701397560146013ad69887c78148ad9b4c
JATeR912 <jater.code@gmail.com>
2025-11-03 20:35:52 -0300

    Modelos y urls en productos

--- empresa/urls.py
+++ empresa/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('productos_app.urls')),
+    path('categorias/', include('categorias_app.urls')),
+    path('etiquetas/', include('etiquetas_app.urls')),
 ]
--- productos_app/models.py
+++ productos_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+    def __str__(self):
+        return self.nombre
 
-# Create your models here.
+class detalle_producto(models.Model):
+    producto = models.OneToOneField(
+        Producto,
+        on_delete=models.CASCADE,
+        related_name='detalle'       
+    )
+    dimension = models.CharField(max_length=100)
+    peso = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return f"Detalle de {self.producto.nombre}"
